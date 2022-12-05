b0f190bf218d14e366c7c075a70476a122d580f8
nick <nicholas.a.carfagno@gmail.com>
2022-12-05 12:55:22 -0500

    updating solution

--- src/App.java
+++ src/App.java
@@ -27,31 +27,30 @@ public class App {
         List<Ball> list3 = ballList.subList(6, 9);
         
         ScalesOfJustice scales = new ScalesOfJustice();
+        Ball heavyBall = new Ball();
+
         List<Ball> heavyList = scales.measureBallList(list1, list2);
         if(heavyList == null){
             //this means these lists dont have a heavy ball. We have one list left, list3!
-            findHeavyBall(scales, list3);
+            heavyBall = findHeavyBall(scales, list3);
+            System.out.println("The heavy ball is ID: " + heavyBall.getId() + ". This ball weighs " + heavyBall.getWeight());
             System.out.println("This operation took " + scales.getMeasurements() + " measurements!");
             System.exit(0);
         }
         else{
-            findHeavyBall(scales, heavyList);
+            heavyBall = findHeavyBall(scales, heavyList);
+            System.out.println("The heavy ball is ID: " + heavyBall.getId() + ". This ball weighs " + heavyBall.getWeight());
             System.out.println("This operation took " + scales.getMeasurements() + " measurements!");
             System.exit(0);
         }
       }
 
-      private static void findHeavyBall(ScalesOfJustice scales, List<Ball> heavyList){
+      private static Ball findHeavyBall(ScalesOfJustice scales, List<Ball> heavyList){
         Ball heavyBall = scales.measureBalls(heavyList.get(0), heavyList.get(1));
         if(heavyBall == null){
             heavyBall = heavyList.get(2);
-            System.out.println("The heavy ball is ID: " + heavyBall.getId() + ". This ball weighs " + heavyBall.getWeight());
-            
         }
-        else{
-            System.out.println("The heavy ball is ID: " + heavyBall.getId() + ". This ball weighs " + heavyBall.getWeight());
-        }
-            
+        return heavyBall;  
       }
 
       private static Ball createBall(int id, int weight){
